10e90ae99ea96ab0018eeb1856598b0cb9fc8add
kaushik-codemonkey <kaushiktechie03@gmail.com>
2023-03-01 13:08:08 +0530

    Finsh api to get the json file with email and pass

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,30 @@
 import express from 'express';
-
+import multer from 'multer';
 const router = express.Router();
 
-const multer = require('multer');
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 6,
+    },
+});
 
 router.get("/health", (req, res) => {
-    res.send("All Okay!")
+    res.send("Endpoint is up!")
 })
 
 
-router.post('/upsync', (req, res) => {
-    console.log("HERE")
+router.post('/upsync', upload.single("jsonFile"), (req, res) => {
+    console.log("HERE", JSON.stringify(req.body))
+    const { email, passphrase } = req.body
+    if (!req.file || req.file.mimetype !== 'application/json') {
+        res.status(400).send("Incorrect input, expected json file in the field 'jsonFile'");
+    }
+    if (!email || !passphrase) {
+        res.status(400).send(`Missing ${!email ? 'email' : !passphrase ? 'passphrase' : 'few required fields'}`)
+    }
     const jsonData = JSON.parse(req.file?.buffer.toString() || '');
     console.log(jsonData);
     res.send('File uploaded successfully');
